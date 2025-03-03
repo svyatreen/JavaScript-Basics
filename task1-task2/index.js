963d9c0ed54faa17c565eec9b5db202cfda64183
svyatoslav <152086601+svyatreen@users.noreply.github.com>
2025-03-04 00:33:24 +0300

    Add files via upload

---
+++ task1-task2/index.js
@@ -0,0 +1,15 @@
+// JavaScript-код с созданием переменных
+/* Автор:
+Лазовский Святослав */
+
+const myName = 'Святослав';
+const mySurname = 'Лазовский';
+let myFavoriteDrink = 'Сок';
+let myFavoriteAnimal = 'Собака';
+let myFavoriteProgrammingLanguage = 'JavaScript';
+
+console.log(myName);
+console.log(mySurname);
+console.log(myFavoriteDrink);
+console.log(myFavoriteAnimal);
+console.log(myFavoriteProgrammingLanguage);
---
+++ task3-task4/car.js
@@ -0,0 +1,21 @@
+const carName = 'Porsche';
+let maxCarSpeed = '300 km/h';
+let carOwner = 'Maxim';
+
+alert(carName);
+alert(maxCarSpeed);
+alert(carOwner);
+
+maxCarSpeed = '280 km/h';
+console.log('У машины изменилась скорость', maxCarSpeed);
+maxCarSpeed = '260 km/h';
+console.log('У машины изменилась скорость', maxCarSpeed);
+maxCarSpeed = '290 km/h';
+console.log('У машины изменилась скорость', maxCarSpeed);
+
+carOwner = 'Andrew';
+console.log('У машины изменился владелец', carOwner);
+carOwner = 'Sam';
+console.log('У машины изменился владелец', carOwner);
+carOwner = 'Michael';
+console.log('У машины изменился владелец', carOwner);
