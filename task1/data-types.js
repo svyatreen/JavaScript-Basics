1d5cc6f5280908eff6e54177ec5421213532e474
svyatoslav <152086601+svyatreen@users.noreply.github.com>
2025-03-04 22:34:37 +0300

    Add files via upload

---
+++ task1/data-types.js
@@ -0,0 +1,27 @@
+const myName = 'Svyatoslav';
+console.log('myName', myName, typeof myName);
+
+const myAge = 19;
+console.log('myAge', myAge, typeof myAge);
+
+const carOwner = undefined;
+console.log('carOwner', carOwner, typeof carOwner);
+
+const iHaveCar = false;
+console.log('iHaveCar', iHaveCar, typeof iHaveCar);
+
+const bigNumber = 200n;
+console.log('bigNumber', bigNumber, typeof bigNumber);
+
+const myFavoriteDrink = null;
+console.log('myFavoriteDrink', myFavoriteDrink, typeof myFavoriteDrink);
+
+const id = Symbol('id');
+console.log('id', id, typeof id);
+
+const car = {
+  carName: 'Porsche',
+  carSpeed: 300,
+  carComfortable: true,
+};
+console.log('car', car, typeof car);
---
+++ task2/index.js
@@ -0,0 +1,11 @@
+let age = 30;
+age = 'Anny';
+alert(age);
+
+let string = 'Andrew';
+string = 32;
+alert(string);
+
+let isColdToday = true;
+isColdToday = null;
+alert(isColdToday);
